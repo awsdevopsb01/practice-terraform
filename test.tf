de26e6e84ff4908a119381023e0c0f0ff95a22de
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-08 20:56:35 +0530

    terraform practice

--- test.tf
+++ test.tf
@@ -1,4 +1,10 @@
 module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.6.0"
+  name="hello"
+  ami = "ami-03265a0778a880afb"
+
+  tags = {
+    Terraform   = "true"
+  }
 }
