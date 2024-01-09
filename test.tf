cd605728579822486b3f8e02836f49c915d612e9
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-09 12:46:30 +0530

    terraform practice create resource

--- test.tf
+++ test.tf
@@ -1,8 +1,9 @@
-module "ec2-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+resource "ec2-instance" "instance" {
+
   version = "5.6.0"
-  name="hello"
+  name = "hello"
   ami = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
 
   tags = {
     Terraform   = "true"
