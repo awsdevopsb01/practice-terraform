99c2d1f61f50b0ee37c5acab321b78b74e96d2c7
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-12 20:24:09 +0530

    loops practice

--- 03-loops/for-each.tf
+++ 03-loops/for-each.tf
@@ -1,12 +1,17 @@
 resource "null_resource" "null" {
   for_each = var.fruits
+
   provisioner "local-exec" {
-    command="echo Fruit name - ${each.key} - ${each.value}"
+    command="echo Fruit name - ${each.key["name"]} - ${each.value["count"]}"
   }
 }
 
 variable "fruits" {
-  apple = 10
+  apple = {
+    name = "apple"
+    count = 10
+  }
+
   banana = 20
   pineapple =30
 }
