5d2cbb79ee521d4f7f9f5720ae6534521ba1a3f2
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-18 21:14:28 +0530

    terraform functions practice

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -7,3 +7,12 @@ variable "test" {
 output "test-disp" {
   value = upper(var.test)
 }
+
+#Print individual elements of a list variable
+variable "test1" {
+  default = ["abc", "123", "xyz"]
+}
+
+output "test-ele" {
+  value = element(var.test1,0)
+}
